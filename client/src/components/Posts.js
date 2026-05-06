7631c4a03a1e53e578ac98792f043b7281da799d
JayantPachori-coder <jayantpachori2004@gmail.com>
2026-05-06 09:57:12 +0530

    vev

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import API from "../../api";
+import API from "../api";
 
 export default function Posts() {
   const [users, setUsers] = useState([]);
